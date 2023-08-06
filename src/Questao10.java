4373ce66225ff2ca9b3157555c9445f75bccb4b3
Rubem Chaves <108533543+vinicchaves@users.noreply.github.com>
2023-08-06 11:58:49 -0300

    Update Questao10.java

--- src/Questao10.java
+++ src/Questao10.java
@@ -2,7 +2,7 @@ public class Questao10 {
     public static void main(String[] args) {
         int x = 6;
         int y = 4;
-        float z = (float) x/y;
+        int z = x/y;
 
         System.out.println(z);
     }
