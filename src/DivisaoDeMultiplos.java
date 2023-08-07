eb8b9bff84810eec2b6be4e1e81e74b2cb789b0d
Rubem Chaves <108533543+vinicchaves@users.noreply.github.com>
2023-08-06 21:55:42 -0300

    code update

--- src/DivisaoDeMultiplos.java
+++ src/DivisaoDeMultiplos.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class DivisaoDeMultiplos {
+public class DivisaoDeMultiplos
+{
+    public static void main(String[] args) {
+        // obtendo os múltiplos de 3 e dividindo por 2, utilizando o int
+        System.out.println("Resultados utilizando int: \n");
+        for (int i = 1; i <= 100 ; i++) {
+            if (i % 3 == 0){
+                int resultadosInt = i/2;
+                System.out.println(resultadosInt);
+
+            }
+
+        }
+        // obtendo os múltiplos de 3 e dividindo por 2, utilizando o double
+        System.out.println("\nResultados utilizando double: \n");
+        for (int j = 1; j <= 100; j++) {
+            if (j % 3 == 0){
+                double resultadosDouble = (double) j/2;
+                System.out.println(resultadosDouble);
+            }
+
+        }
+    }
 }
